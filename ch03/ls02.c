e3ad2d04f1d3200e3d87e76e29acd7878ca1f16c
HuSharp <19973658602@163.com>
2020-11-27 18:33:38 +0800

    easy ls02.c finished

--- ch03/ls02.c
+++ ch03/ls02.c
@@ -1,41 +1,139 @@
-/** ls1.c
- **   purpose  list contents of directory or directories
- **   action   if no args, use .  else list files in args
- **/
+// ls02.c
+//    purpose  list contents of directory or directories
+//    action   if no args, use .  else list files in args
+//    note     uses stat and pwd.h and grp.h
 
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 void do_ls(char dirname[]);
-// ls 
-int main(int argc, char** argv) 
+void do_stat(char*); 
+void mod_to_letters(int, char*);
+char* uid_to_name(uid_t);
+char* gid_to_name(gid_t);
+
+// ls
+int main(int argc, char** argv)
 {
-    if(argc == 1) {
+    if (argc == 1) {
         do_ls(".");
     } else {
-        while(--argc) {
+        while (--argc) {
             printf("%s:\n", *++argv);
             do_ls(*argv);
         }
     }
-    return 0;
-} 
+    return EXIT_SUCCESS;
+}
+
 
 // list files in directory called dirname
-// 就是循环调用 readdir ，对 通过  opendir 打开 dirname 的 dir_ptr 不断进行读出 
-void do_ls(char dirname[]) {
-    DIR *dir_ptr;
-    struct dirent * dirent_pt;// each entry
+// 就是循环调用 readdir ，对 通过  opendir 打开 dirname 的 dir_ptr 不断进行读出
+void do_ls(char dirname[])
+{
+    DIR* dir_ptr;
+    struct dirent* dirent_pt; // each entry
 
-    if((dir_ptr = opendir(dirname)) == NULL) {
-        fprintf(stderr, "ls1: cannot open %s\n", dirname);
-    } else {// open sucessful
-        while((dirent_pt = readdir(dir_ptr)) != NULL) {
-            printf("%s\n", dirent_pt->d_name);
+    if ((dir_ptr = opendir(dirname)) == NULL) {
+        fprintf(stderr, "ls2: cannot open %s\n", dirname);
+    } else { // open sucessful
+        while ((dirent_pt = readdir(dir_ptr)) != NULL) {
+            do_stat(dirent_pt->d_name);
+            // printf("%s\n", dirent_pt->d_name);
         }
         closedir(dir_ptr);
     }
+    return EXIT_SUCCESS;
+}
+
+
+
+void do_stat(char *filename) {
+    struct stat stat_buf;
+    
+    if(stat(filename, &stat_buf) == -1) {
+        perror(filename);
+    } else {
+        show_stat_info(filename, &stat_buf);
+    }
+}
+
+/*
+总用量 16
+-rw-r--r-- 1 husharp husharp 1182 9月  27 00:24 git需知
+drwxr-xr-x 8 husharp husharp 4096 11月 26 10:46 Linux
+*/
+void show_stat_info(char* filename, struct stat* buf)
+{
+    char modeStr[11];
+    mod_to_letters(buf->st_mode, modeStr);// mode change
+
+    printf("%s ", modeStr);
+    printf("%4d ", (int)buf->st_nlink);
+    printf("%-8s ", uid_to_name(buf->st_uid));// 靠左对齐
+    printf("%-8s ", gid_to_name(buf->st_uid));
+    printf("%8ld ", (long)buf->st_size);// long int
+    // ctime
+    // printf("%.12s")
+    printf("%s\n", filename);
+}
+
+
+// chansform mode to char array the file type
+void mod_to_letters(int mode, char* str)
+{
+    strcpy(str, "----------"); // default
+    // 3 bits for user
+    if (S_ISDIR(mode)) // 对应 reg 文件就直接 default 为 - 了
+        str[0] = 'd';
+    if (S_ISCHR(mode))
+        str[0] = 'c';
+    if (S_ISBLK(mode))
+        str[0] = 'b';
+    // 3 bits for group
+    if (mode & S_IRUSR)
+        str[1] = 'r';
+    if (mode & S_IWUSR)
+        str[2] = 'w';
+    if (mode & S_IXUSR)
+        str[3] = 'x';
+    // 3 bits for other
+    if (mode & S_IRGRP)
+        str[4] = 'r';
+    if (mode & S_IWGRP)
+        str[5] = 'w';
+    if (mode & S_IXGRP)
+        str[6] = 'x';
+}
+
+//  The passwd structure.
+// struct passwd
+// {
+//   char *pw_name;		/* Username.  */
+//   char *pw_passwd;		/* Password.  */
+//   __uid_t pw_uid;		/* User ID.  */
+//   __gid_t pw_gid;		/* Group ID.  */
+//   char *pw_gecos;		/* Real name.  */
+//   char *pw_dir;			/* Home directory.  */
+//   char *pw_shell;		/* Shell program.  */
+// };
+
+#include <pwd.h>
+// 通过 stat 返回的 uid 得到 uid name
+char* uid_to_name(uid_t uid)
+{
+    return getpwuid(uid)->pw_name;
+}
+
+// 通过 stat 返回的 gid 得到 gid name
+#include <grp.h>
+char* gid_to_name(gid_t gid)
+{
+    return getgrgid(gid)->gr_name;
 }
--- ch03/think.md
+++ ch03/think.md
@@ -89,12 +89,169 @@ struct stat {
 }
 ```
 
+实现完  fileinfo  之后，目前对比如下
+
+```
+husharp@hjh-Ubuntu:~/CS/Unix-Linux/Unix&Linux_Programming/ch03$ ./fileinfo fileinfo.c    mode: 100664
+  links: 1
+   user: 1000
+  group: 1000
+   size: 1461
+modtime: 1606406362
+   name: fileinfo.c
 
 
+husharp@hjh-Ubuntu:~/CS/Unix-Linux/Unix&Linux_Programming/ch03$ ls -l fileinfo.c 
+-rw-rw-r-- 1 husharp husharp 1461 11月 26 23:59 fileinfo.c
+```
 
+发现 links、size、name 没问题，ctime可以转换 modtime
 
-
-
-
+### 现在需要做的是：
 
 ![image-20201126233316127](/home/husharp/CS/Unix-Linux/Unix&Linux_Programming/ch03/think.assets/image-20201126233316127.png)
+
+mode 的转换 和 uid gid 的数字--> 用户名
+
+100664 与 -rw-rw-r-- 的关系：
+
+![image-20201127170133305](/home/husharp/CS/Unix-Linux/Unix&Linux_Programming/ch03/think.assets/image-20201127170133305.png)
+
+st_mode 长 16 位，按照每三位进行 100664 = 001,000,000,110,110,100
+
+因此 type 就是 1000，其余依次
+
+在 sys/stats.h 中，发现是通过掩码的方式进行文件类型分类
+
+```
+/* File types.  */
+#define	__S_IFDIR	0040000	/* Directory.  */
+#define	__S_IFCHR	0020000	/* Character device.  */
+#define	__S_IFBLK	0060000	/* Block device.  */
+#define	__S_IFREG	0100000	/* Regular file.  */
+#define	__S_IFIFO	0010000	/* FIFO.  */
+#define	__S_IFLNK	0120000	/* Symbolic link.  */
+#define	__S_IFSOCK	0140000	/* Socket.  */
+```
+
+因此上面的 100664 明显为 regular 
+
+#### 1、mode 转换为 str
+
+现在实现转换 mode
+
+```c
+// chansform mode to char array the file type
+void mod_to_letters(int mode, char* str)
+{
+    strcpy(str, "----------"); // default
+    // 3 bits for user
+    if (S_ISDIR(mode)) // 对应 reg 文件就直接 default 为 - 了
+        str[0] = 'd';
+    if (S_ISCHR(mode))
+        str[0] = 'c';
+    if (S_ISBLK(mode))
+        str[0] = 'b';
+    // 3 bits for group
+    if (mode & S_IRUSR)
+        str[1] = 'r';
+    if (mode & S_IWUSR)
+        str[2] = 'w';
+    if (mode & S_IXUSR)
+        str[3] = 'x';
+    // 3 bits for other
+    if (mode & S_IRGRP)
+        str[4] = 'r';
+    if (mode & S_IWGRP)
+        str[5] = 'w';
+    if (mode & S_IXGRP)
+        str[6] = 'x';
+}
+```
+
+
+
+#### 2、uid
+
+联机查看 username
+
+```
+getlogin (3)         - get username
+```
+
+查看  more /etc/passwd
+
+```
+root:x:0:0:root:/root:/bin/bash
+daemon:x:1:1:daemon:/usr/sbin:/usr/sbin/nologin
+```
+
+但是 /etc/passwd 并没有包含所有用户，而是通过 NIS 保存所有用户信息
+
+man 3 getpwuid 可以访问用户信息得到 passwd 指针
+
+```c
+   The passwd structure is defined in <pwd.h> as follows:
+
+       struct passwd {
+           char   *pw_name;       /* username */
+           char   *pw_passwd;     /* user password */
+           uid_t   pw_uid;        /* user ID */
+           gid_t   pw_gid;        /* group ID */
+           char   *pw_gecos;      /* user information */
+           char   *pw_dir;        /* home directory */
+           char   *pw_shell;      /* shell program */
+       };
+```
+因此可以通过该结构得到 name
+
+```c
+#include <pwd.h>
+// 通过 stat 返回的 uid 得到 uid name
+char *uid_to_name( uid_t uid) {
+    return getpwuid(uid)->pw_name;
+}
+```
+
+
+
+#### 3、gid
+
+```
+man 5 group
+-------------------------------
+group - user group file
+
+DESCRIPTION
+       The  /etc/group file is a text file that defines the groups on the system.  There is one entry per line, with the following format:
+           group_name:password:GID:user_list
+```
+
+cat /etc/group
+
+```
+root:x:0:
+daemon:x:1:
+bin:x:2:
+sys:x:3:
+cdrom:x:24:husharp
+plugdev:x:46:husharp
+```
+
+最后一个是组中的成员列表， 一个用户可以同时属于多个组
+
+**getgrgid 函数可以得到组名，而不用操心实现的细节。**
+
+```c
+#include <grp.h>
+char *gid_to_name( gid_t gid) {
+    return getgrgid(gid)->gr_name;
+}
+```
+
+综合可得具体函数见 ls02.c
+
+
+
+
+
